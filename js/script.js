10493778aabfbdc344d463ff0bb9f91d27db22a3
Black Shadow <mithabozdar2@gmail.com>
2024-12-03 01:48:44 +0500

    Add javascript to handle UI to library connection

--- js/script.js
+++ js/script.js
@@ -17,28 +17,35 @@ cancelDialog.addEventListener("click", (event) => {
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
+    this.id = Book.counter++;
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
+Book.counter = 0;
+
 
 theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
 addBookToLibrary(theHobbit);
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
-    pushBookToUI(book, myLibrary.indexOf(book));
+    pushBookToUI(book, book.id);
 }
 
-function removeBookFromLibrary(index) {
-    myLibrary.splice(index, 1);
+function removeBookFromLibrary(identifier) {
+    myLibrary.forEach((item) => {
+        if (item.id === identifier) {
+            myLibrary.splice(myLibrary.indexOf(item), 1);
+        }
+    });
 }
 
 // Code to handle connecting Library to UI
-function pushBookToUI(book, index) {
+function pushBookToUI(book, identifier) {
     const clone = template.content.cloneNode(true);
-    clone.querySelector("#book-remove").setAttribute("data-index", index);
+    clone.querySelector("#book-remove").setAttribute("data-identifier", identifier);
     clone.querySelector(".card-title").textContent = book.title;
     clone.querySelector(".author-name").textContent = book.author;
     clone.querySelector(".pages").textContent = book.pages;
@@ -46,11 +53,20 @@ function pushBookToUI(book, index) {
     cardContainer.insertBefore(clone, cardContainer.firstChild);
 }
 
-if (myLibrary.length !== 0) {
-    const cardRemove = document.querySelector("#book-remove");
-
-    cardRemove.addEventListener("click", (event) => {
-        removeBookFromLibrary(event.target.getAttribute("data-index"));
+cardContainer.addEventListener("click", (event) => {
+    if (event.target.id === "book-remove") {
+        removeBookFromLibrary(parseInt(event.target.getAttribute("data-identifier"))); // Always convert to numerical form
         event.target.parentNode.remove();
-    });
-}
+    }
+});
+
+// Code to handle getting date from UI to Library
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+    const { title, author, pages, read } = form.elements;
+    const newBook = new Book(title.value, author.value, parseInt(pages.value), read.checked);
+    addBookToLibrary(newBook);
+
+    form.reset();
+});
